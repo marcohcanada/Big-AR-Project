8c4e12c6ab4598e421bdef61520a994b230e1c3c
Marco Hidalgo-Romero <marcohcanada@gmail.com>
2023-05-25 23:20:49 -0400

    Add files via upload

---
+++ Assets/VideoPlayback.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayback : MonoBehaviour
+{
+    public VideoClip[] videoClips;
+    private VideoPlayer videoPlayer;
+    private int videoClipIndex;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.clip = videoClips[0];
+    }
+
+    public void PlayNext()
+    {
+        videoClipIndex++;
+
+        if (videoClipIndex >= videoClips.Length)
+        {
+            videoClipIndex = videoClipIndex % videoClips.Length;
+        }
+
+        videoPlayer.clip = videoClips[videoClipIndex];
+    }
+
+    public void PlayPrevious()
+    {
+        if (videoClipIndex != 0)
+        {
+            videoClipIndex--;
+        }
+        else
+        {
+            videoClipIndex = videoClips.Length - 1;
+        }
+
+        if (videoClipIndex >= videoClips.Length)
+        {
+            videoClipIndex = videoClipIndex % videoClips.Length;
+        }
+
+        videoPlayer.clip = videoClips[videoClipIndex];
+    }
+
+    public void PlayPause()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        }
+        else
+        {
+            videoPlayer.Play();
+        }
+    }
+}
